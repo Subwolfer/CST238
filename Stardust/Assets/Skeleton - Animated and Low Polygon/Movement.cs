57f457dfb54cdb6897911e86a6961d211b5ac629
Rie Kumar <kingverte@gmail.com>
2016-04-11 07:53:37 -0700

    Skeleton Attack Animations

--- Stardust/Assets/Skeleton - Animated and Low Polygon/Movement.cs
+++ Stardust/Assets/Skeleton - Animated and Low Polygon/Movement.cs
@@ -4,13 +4,11 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
     static Animator anim;
+    public double time;
     public float speed = 0.0f;
     public float walk = 10.0f;
     public float sprint = 15.0f;
     public float rotate = 5.0f;
-    public float horizontal = Input.GetAxis("Horizontal");
-    public float vertical = Input.GetAxis("Vertical");
-    public bool move;
 
 	// Use this for initialization
 	void Start ()
@@ -21,14 +19,14 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        //transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        time += Time.deltaTime;
 
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
         transform.Rotate(0, horizontal * rotate, 0);
 
-        if (vertical != 0 && !anim.GetBool("Normal_Attack")) 
+        if (vertical != 0 && !anim.GetBool("Quick_Attack") && !anim.GetBool("Normal_Attack")) 
         {
             if (Input.GetKey(KeyCode.LeftShift))
                 speed = sprint;
@@ -50,7 +48,15 @@ public class Movement : MonoBehaviour {
 
             if (Input.GetKey(KeyCode.Space))
             {
-                anim.SetBool("Normal_Attack", true);
+                if (Random.Range(0, 10) <= 3)
+                {
+                    anim.SetBool("Quick_Attack", true);
+                }
+                else
+                {
+                    anim.SetBool("Normal_Attack", true);
+                }
+
                 anim.SetBool("Running", false);
                 anim.SetBool("Walking", false);
             }
@@ -61,9 +67,28 @@ public class Movement : MonoBehaviour {
             anim.SetBool("Walking", false);
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            anim.SetBool("Normal_Attack", true);
+            time = 0;
+
+            if (Random.Range(0, 10) <= 3)
+            {
+                anim.SetBool("Quick_Attack", true);
+            }
+            else
+            {
+                anim.SetBool("Normal_Attack", true);
+            }
+        }
+
+        if (time > 1.07)
+        {
+            anim.SetBool("Quick_Attack", false);
+
+            if (time > 3)
+            {
+                anim.SetBool("Normal_Attack", false);
+            }
         }
 	}
 }
